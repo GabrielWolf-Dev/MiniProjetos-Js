dbd28127948c8f49d32d41e3be8372307f062425
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2020-10-31 11:19:17 -0300

    Classificação do IMC
    Inserindo a Classificação do IMC:
    - Abaixo do peso;
    - Peso ideal;
    - Levemente acima do peso;
    - Obesidade grau 1;
    - Obesidade grau 2;
    - Obesidade grau 3.

--- IMC/main.js
+++ IMC/main.js
@@ -6,6 +6,22 @@ function calculoIMC() {
 
     let imc = peso / (altura * altura);
 
-    resultado.innerHTML = ` <p> O seu IMC é ${imc.toFixed(2)} </p> `;
+    resultado.innerHTML += ` <p> O seu IMC é ${imc.toFixed(2)}. </p> `;
+
+    if(imc < 18.5) {
+        resultado.innerHTML += `<p> Sua classificação: Você está abaixo do peso. </p>`;
+    }else if (imc >= 18.6 && imc <= 24.9) {
+        resultado.innerHTML += `<p> Sua classificação: Você está no peso ideal. </p>`;
+    }else if(imc >= 25 && imc <= 29.9) {
+        resultado.innerHTML += `<p> Sua classificação: Você está levemente acima do peso. </p>`;
+    }else if (imc >= 30 && imc <= 34.9) {
+        resultado.innerHTML += `<p> Sua classificação: Você está na Obesidade grau 1.</p>`;
+    }else if (imc >= 35 && imc <= 39.9) {
+        resultado.innerHTML += `<p> Sua classificação: Você está na Obesidade grau 2.</p>`;
+    }else {
+        resultado.innerHTML += `<p> Sua classificação: Você está na Obesidade grau 3.</p>`;
+    }
+
+
 
 };
