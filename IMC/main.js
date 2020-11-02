cfa622801a3ec07480b3feb94327d6b84f821e96
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2020-11-02 16:36:23 -0300

    Duplicação de resultados
    Retirando a duplicação dos resultados

--- IMC/main.js
+++ IMC/main.js
@@ -6,22 +6,18 @@ function calculoIMC() {
 
     let imc = peso / (altura * altura);
 
-    resultado.innerHTML += ` <p> O seu IMC é ${imc.toFixed(2)}. </p> `;
-
     if(imc < 18.5) {
-        resultado.innerHTML += `<p> Sua classificação: Você está abaixo do peso. </p>`;
+        resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está abaixo do peso. </p>`;
     }else if (imc >= 18.6 && imc <= 24.9) {
-        resultado.innerHTML += `<p> Sua classificação: Você está no <span>peso ideal</span>. </p>`;
+        resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está no <span>peso ideal</span>. </p>`;
     }else if(imc >= 25 && imc <= 29.9) {
-        resultado.innerHTML += `<p> Sua classificação: Você está <span>levemente acima do peso</span>. </p>`;
+        resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está <span>levemente acima do peso</span>. </p>`;
     }else if (imc >= 30 && imc <= 34.9) {
-        resultado.innerHTML += `<p> Sua classificação: Você está na <span>Obesidade grau 1.</span></p>`;
+        resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está na <span>Obesidade grau 1.</span></p>`;
     }else if (imc >= 35 && imc <= 39.9) {
-        resultado.innerHTML += `<p> Sua classificação: Você está na <span>Obesidade grau 2.</span></p>`;
+        resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está na <span>Obesidade grau 2.</span></p>`;
     }else {
-        resultado.innerHTML += `<p> Sua classificação: Você está na <span>Obesidade grau 3.</span></p>`;
+        resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está na <span>Obesidade grau 3.</span></p>`;
     }
-
-
 
 };
