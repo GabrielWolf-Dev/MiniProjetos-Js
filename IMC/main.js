2909eaba067c55a3839da702569660e54fe2b393
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2020-11-02 16:28:16 -0300

    Estilização
    Adicionando o design do Projeto IMC

--- IMC/main.js
+++ IMC/main.js
@@ -11,15 +11,15 @@ function calculoIMC() {
     if(imc < 18.5) {
         resultado.innerHTML += `<p> Sua classificação: Você está abaixo do peso. </p>`;
     }else if (imc >= 18.6 && imc <= 24.9) {
-        resultado.innerHTML += `<p> Sua classificação: Você está no peso ideal. </p>`;
+        resultado.innerHTML += `<p> Sua classificação: Você está no <span>peso ideal</span>. </p>`;
     }else if(imc >= 25 && imc <= 29.9) {
-        resultado.innerHTML += `<p> Sua classificação: Você está levemente acima do peso. </p>`;
+        resultado.innerHTML += `<p> Sua classificação: Você está <span>levemente acima do peso</span>. </p>`;
     }else if (imc >= 30 && imc <= 34.9) {
-        resultado.innerHTML += `<p> Sua classificação: Você está na Obesidade grau 1.</p>`;
+        resultado.innerHTML += `<p> Sua classificação: Você está na <span>Obesidade grau 1.</span></p>`;
     }else if (imc >= 35 && imc <= 39.9) {
-        resultado.innerHTML += `<p> Sua classificação: Você está na Obesidade grau 2.</p>`;
+        resultado.innerHTML += `<p> Sua classificação: Você está na <span>Obesidade grau 2.</span></p>`;
     }else {
-        resultado.innerHTML += `<p> Sua classificação: Você está na Obesidade grau 3.</p>`;
+        resultado.innerHTML += `<p> Sua classificação: Você está na <span>Obesidade grau 3.</span></p>`;
     }
 
 
