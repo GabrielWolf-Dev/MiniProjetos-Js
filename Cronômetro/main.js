a718ecd7122a340b5303a7090c087dcf99216ab2
GabrielWolf-Dev <65361452+GabrielWolf-Dev@users.noreply.github.com>
2020-11-14 23:01:17 -0300

    Script do Cronômetro finalizado!
    Terminamos o script do Cronômetro e inserimos o HTML para interagir.

---
+++ Cronômetro/main.js
@@ -0,0 +1,35 @@
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+let btn = document.querySelector('.btn');
+let result = document.querySelector('.result');
+
+
+btn.addEventListener('click', () => {
+
+    let minAtual = Number(min.value);
+    let secAtual = Number(sec.value);
+
+    if(minAtual === 0 && secAtual === 0) {
+        alert('Por favor, insira um tempo para começar a contagem');
+    }else {
+
+        let  interval = setInterval( () => {
+
+            secAtual--;
+            if(secAtual <= 0) {
+                if(minAtual > 0) {
+                    minAtual--;
+                    secAtual = 59;
+                }else {
+                    clearInterval(interval);
+                    document.querySelector('.sound').play();
+                    result.innerHTML += `<p style="color: blue; font-weight: bold;">Acabou o tempo!</p>`;
+                }
+            }
+
+            result.innerHTML += `<p style="color:red;">${minAtual}:${secAtual}</p>`;
+        }, 1000);
+
+    }
+
+});
