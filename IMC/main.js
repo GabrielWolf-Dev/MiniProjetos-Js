7dedcadcf5a25f6bd98255004b76c42460e280e2
GabrielWolf-Dev <65361452+GabrielWolf-Dev@users.noreply.github.com>
2020-11-27 22:07:25 -0300

    Inserindo as boas práticas
    Foi colocado como boa prática todas as variáceis e métodos em inglês

--- IMC/main.js
+++ IMC/main.js
@@ -1,35 +1,31 @@
-function calculoIMC() {
+function calculation() {
 
-    let peso = Number(document.querySelector('#peso').value);
-    let altura = Number(document.querySelector('#altura').value);
-    let resultado = document.querySelector('#resultadoIMC');
+    let weight = Number(document.querySelector('#weight').value);
+    let height = Number(document.querySelector('#height').value);
+    let result = document.querySelector('#resultIMC');
 
-    if(peso === 0 || altura === 0) {
+    if(weight === 0 || height === 0) {
 
         alert("Por favor, insira os dados de acordo com o que é solicitado.");
 
     }else {
 
-
-        let imc = peso / (altura * altura);
+        let imc = weight / (height * height);
 
         if(imc < 18.5) {
-            resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está abaixo do peso. </p>`;
+            result.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está abaixo do peso. </p>`;
         }else if (imc >= 18.6 && imc <= 24.9) {
-            resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está no <span>peso ideal</span>. </p>`;
+            result.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está no <span>peso ideal</span>. </p>`;
         }else if(imc >= 25 && imc <= 29.9) {
-            resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está <span>levemente acima do peso</span>. </p>`;
+            result.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está <span>levemente acima do peso</span>. </p>`;
         }else if (imc >= 30 && imc <= 34.9) {
-            resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está na <span>Obesidade grau 1.</span></p>`;
+            result.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está na <span>Obesidade grau 1.</span></p>`;
         }else if (imc >= 35 && imc <= 39.9) {
-            resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está na <span>Obesidade grau 2.</span></p>`;
+            result.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está na <span>Obesidade grau 2.</span></p>`;
         }else {
-            resultado.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está na <span>Obesidade grau 3.</span></p>`;
+            result.innerHTML = `<p> O seu IMC é ${imc.toFixed(2)}. </p> <p> Sua classificação: Você está na <span>Obesidade grau 3.</span></p>`;
         }
 
-
-
     }
-
-    
+   
 };
